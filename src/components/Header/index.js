1e6897a48f6d84a0d34cb930ca214b259d4ca264
Joaquin Lacosta <joacolacosta2001@gmail.com>
2021-05-18 17:31:33 -0300

    added header styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,15 @@
 import React from "react"
-import { Link } from "react-router-dom"
-import { HeaderWrapper } from "./styles"
+import { HeaderWrapper, HeaderLogo, HeaderButton } from "./styles"
 
 
 const Header = () => {
   return(
     <HeaderWrapper>
-      <div className="Header__logo">
-        <h1>SuperBlog</h1>
+      <div>
+        <HeaderLogo href="/">SuperBlog</HeaderLogo>
       </div>
-      <div className="Header__button">
-        <Link to="/posts/create">New post</Link>
+      <div>
+        <HeaderButton href="/posts/create">New post</HeaderButton>
       </div>
     </HeaderWrapper>
   )
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,6 +3,25 @@ import styled from "styled-components"
 export const HeaderWrapper = styled.header`
   width: 100%;
   display: flex;
-  padding: 5px;
+  padding: 0 10px;
+  height: 80px;
   justify-content: space-between;
+  align-items: center;
+  background-color: #191716;
+  color: white;
+`
+
+export const HeaderLogo = styled.a`
+  text-decoration: none;
+  color: white;
+  font-size: 32px;
+  font-weight: 700
+`
+
+export const HeaderButton = styled.a`
+  text-decoration: none;
+  color: white;
+  padding: 8px;
+  background-color: #3D348B;
+  border-radius: 5px
 `
